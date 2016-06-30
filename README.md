f684f6994c5f232e5dd20da89fd4a066a39ebf80
SL Potapenko <spot62@yandex.ru>
2016-06-30 11:42:48 +0300

    Adding project-specific patches for uboot

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # nuc970buildroot
 Custom buildroot project for VT300(NUC97x)
+
+Building:
+<pre>
+  ./configure.sh
+  ./build.sh
+</pre>
+
+for more information use:
+<pre>
+  ./build.sh help
+</pre>
+
+
+
--- configure.sh
+++ configure.sh
@@ -8,7 +8,7 @@ DEFCONFIG_FILE=${CONTRIB_PATH}/configs/vt300_defconfig
 # uncomment bellow :
 
 # 1) defconfig
-#make BR2_EXTERNAL=${CONTRIB_PATH} -C ${BUILDROOT_PATH} O=${OUTPUT_PATH} BR2_DEFCONFIG=${DEFCONFIG_FILE} defconfig
+make BR2_EXTERNAL=${CONTRIB_PATH} -C ${BUILDROOT_PATH} O=${OUTPUT_PATH} BR2_DEFCONFIG=${DEFCONFIG_FILE} defconfig
 
 # 2) savedefconfig
 #make -C ${BUILDROOT_PATH} O=${OUTPUT_PATH} BR2_DEFCONFIG=${DEFCONFIG_FILE} savedefconfig
