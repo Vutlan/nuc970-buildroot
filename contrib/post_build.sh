c02bed728cfe00157e8a264ce5deab905bc7a1e1
SL Potapenko <spot62@yandex.ru>
2017-07-31 10:05:29 +0300

    Changing the environment configuration

--- contrib/post_build.sh
+++ contrib/post_build.sh
@@ -3,6 +3,7 @@
 echo "Script for prebuild for target: ${TARGET_DIR}"
 
 rm -f ${TARGET_DIR}/etc/init.d/S50dropbear
+rm -f ${TARGET_DIR}/etc/init.d/S60openvpn
 rm -f ${TARGET_DIR}/etc/nginx/*.default 
 
 ln -sf ../opt/xmon/etc/localtime ${TARGET_DIR}/etc/localtime
