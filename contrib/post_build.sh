a3f53f216e7bcfaaea4d07a8b9d22cf2f1fe0981
gerner-terner <gernerterner@gmail.com>
2019-07-12 10:50:16 +0300

    fix SD card support on VT825, kernel version stamp: 11.07.2019

--- contrib/post_build.sh
+++ contrib/post_build.sh
@@ -9,7 +9,8 @@ echo "*************************************************"
 #KERNEL_VERSION_STAMP="12.03.2019"
 #KERNEL_VERSION_STAMP="30.04.2019"
 #KERNEL_VERSION_STAMP="20.05.2019"
-KERNEL_VERSION_STAMP="17.06.2019"
+#KERNEL_VERSION_STAMP="17.06.2019"
+KERNEL_VERSION_STAMP="11.07.2019"
 
 # push kernel version stamp
 echo "    Vutlan kernel version stamp: ${KERNEL_VERSION_STAMP}"
