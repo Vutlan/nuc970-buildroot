268f54b1aff027d4d16bb79d5235c573c2b9409f
gernerterner <gernerterner@gmail.com>
2020-11-06 17:31:01 +0300

    nginx new init

--- contrib/updater/make_update.sh
+++ contrib/updater/make_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "*************************************************"
-echo "* Script for make system update (script ver.2.6)"
+echo "* Script for make system update (script ver.2.7)"
 echo "*   tools: ${BR2_EXTERNAL}/updater"
 echo "*   rootfs: ${TARGET_DIR}"
 echo "*   output: ${BASE_DIR}"
@@ -18,6 +18,7 @@ rsync -avzh ${BR2_EXTERNAL}/updater ${BASE_DIR} > /dev/null 2>&1
 mkdir -p ${BASE_DIR}/updater/rootfs/opt/xmon
 mkdir -p ${BASE_DIR}/updater/rootfs/opt/xmon/etc
 mkdir -p ${BASE_DIR}/updater/rootfs/opt/xmon/etc/radius
+mkdir -p ${BASE_DIR}/updater/rootfs/opt/xmon/etc/nginx
 
 # dirs for system
 mkdir -p ${BASE_DIR}/updater/rootfs/bin
@@ -42,6 +43,10 @@ rsync -avzh ${TARGET_DIR}/etc/init.d ${BASE_DIR}/updater/rootfs/etc > /dev/null 
 
 # busybox update file - now in kernel patch
 #cp -f ${TARGET_DIR}/bin/busybox ${BASE_DIR}/updater/rootfs/bin/busybox > /dev/null 2>&1
+
+# update nginx server config files
+cp -f ${TARGET_DIR}/opt/xmon/etc/nginx/webui2 ${BASE_DIR}/updater/rootfs/opt/xmon/etc/nginx/webui2 > /dev/null 2>&1
+cp -f ${TARGET_DIR}/opt/xmon/etc/nginx/webui_safemode ${BASE_DIR}/updater/rootfs/opt/xmon/etc/nginx/webui_safemode > /dev/null 2>&1
 
 # mjpg_streamer update files
 cp -f ${TARGET_DIR}/usr/bin/mjpg_streamer ${BASE_DIR}/updater/rootfs/usr/bin/mjpg_streamer > /dev/null 2>&1
